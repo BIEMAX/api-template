6ace3a1ce4367b3e2888173182a153995177bd67
BIEMAX <tecnico.beilke@gmail.com>
2021-03-11 23:36:23 -0300

    Added waterfall lib

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const config = require('./config/index')
 
-app.listen(config.api.port || 3000, function() {
-    console.log('----------------------------------------------------------------------')
+app.listen(config.api.port || 3000, function () {
+    console.log('-------------------------------------------------------------------------------------------------')
     console.log(`API server started in port ${config.api.port || 3000} in ${Date()}`)
-    console.log('----------------------------------------------------------------------')
+    console.log('-------------------------------------------------------------------------------------------------')
 })
