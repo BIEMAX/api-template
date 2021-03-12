3fe14dd93182f6dc21cdd91b68ebf40897cb276f
BIEMAX <tecnico.beilke@gmail.com>
2021-03-11 21:54:42 -0300

    Some tests

--- src/libs/customError.js
+++ src/libs/customError.js
@@ -6,6 +6,6 @@ module.exports = (err) => {
     let error = {}
 
     error.status = err.status ? err.status : 400
-    // TODO: #1 Implement i18n to support multiple languages
+    // TODO: Implement i18n to support multiple languages.
     error.message = err.message ? err.message : 'Sorry, something was wrong'
 }
