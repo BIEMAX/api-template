a9075b6c4ed0fa0aa5f5c0aa61769df347b400db
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2021-09-22 17:04:37 -0300

    Solved the issue in swagger tags was already expanded

--- src/app/v2/apidoc/index.js
+++ src/app/v2/apidoc/index.js
@@ -13,6 +13,9 @@ module.exports = () => {
     definition: apiJsonV2
   })
 
+  //For 'customOptions.swaggerOptions' definitions, read the documentation in the following link:
+  //https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
+
   const customOptions = {
     customSiteTitle: config.api.applicationName,
     customCss: '.swagger-ui .topbar { display: none }',
@@ -21,14 +24,15 @@ module.exports = () => {
     explorer: false,
     filter: true,
     swaggerOptions: {
-      //https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
-      displayRequestDuration: true,
-      filter: true,
-      showExtensions: true,
-      showCommonExtensions: true,
+      deepLinking: false,
       displayOperationId: true,
-      docExpansion: 'none', //Define if the documentation already start expanded || //Controls the default expansion setting for the operations and tags. It can be 'list' (expands only the tags), 'full' (expands the tags and operations) or 'none' (expands nothing).
-      apisSorter: 'alpha',
+      defaultModelsExpandDepth: -1,
+      defaultModelExpandDepth: 1,
+      //defaultModelRendering: 'model',
+      displayRequestDuration: true,
+      docExpansion: 'none', //'list', 'full' or 'none'
+      filter: true,
+      maxDisplayedTags: 0,
       operationsSorter: (a, b) => {
         let methodsOrder = [
           'post',
@@ -39,20 +43,27 @@ module.exports = () => {
           'options',
           'trace'
         ]
-        let result =
-          methodsOrder.indexOf(a.get('method')) -
-          methodsOrder.indexOf(b.get('method'))
+        let result = methodsOrder.indexOf(a.get('method')) - methodsOrder.indexOf(b.get('method'))
 
-        if (result === 0) {
-          result = a.get('path').localeCompare(b.get('path'))
-        }
-
+        if (result === 0) result = a.get('path').localeCompare(b.get('path'))
         return result
-      }
+      },
+      showExtensions: true,
+      showCommonExtensions: true,
+      //tagsSorter: '',
+      useUnsafeMarkdown: false,
+      //onComplete: '',
+      syntaxHighlight: {
+        activate: false,
+        theme: '', //String=["agate" *, "arta", "monokai", "nord", "obsidian", "tomorrow-night"].Highlight.js syntax coloring theme to use. (Only these 6 styles are available.)
+      },
+      tryItOutEnabled: true,
+      //requestSnippets: '',
+      apisSorter: 'alpha',
     }
   }
 
-  router.use('/', swaggerUi.serve)
+  router.use('/', swaggerUi.serveFiles(swaggerDocumentsSpecification, customOptions))
   router.get('/', swaggerUi.setup(swaggerDocumentsSpecification, customOptions, null, null, null, null, config.api.applicationName))
 
   return router
