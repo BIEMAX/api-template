62335150f32e70f2d9ce4922eb3bb9ac2b25ae64
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2021-10-05 18:45:22 -0300

    Added query cosmos db module exports

--- src/libs/azureCosmos.js
+++ src/libs/azureCosmos.js
@@ -10,7 +10,7 @@ const database = client.database(config.azure.database)
  * @param {Object} body Json object (body) with parameters to insert in database
  * @returns {Boolean} True if success, false otherwise
  */
-module.exports.insertCosmosdb = (container, body) => {
+module.exports.insert = (container, body) => {
   return new Promise((resolve, reject) => {
     waterfall(
       [
@@ -22,11 +22,42 @@ module.exports.insertCosmosdb = (container, body) => {
               resolve({
                 status: true,
                 message: translate('lib.azure.success'),
-                data: {
+                cosmosData: {
                   cosmosId: saveAzure.item.id
                 }
               })
             } else reject(translate('lib.azure.error'), saveAzure)
+          } catch (err) {
+            reject(err)
+          }
+        }
+      ],
+      (err) => {
+        reject(err)
+      }
+    )
+  })
+}
+
+/**
+ * Query data in the CosmosDB
+ * @param {String} container Container name
+ * @param {String} query Query to run (SELECT * FROM database as d WHERE d.column = 'value')
+ * @return {Object} Data from cosmosDb as object
+ */
+module.exports.query = (container, query) => {
+  return new Promise((resolve, reject) => {
+    waterfall(
+      [
+        async () => {
+          try {
+            let cosmosContainer = database.container(container)
+            let data = await cosmosContainer.items.query({ query: query }).fetchAll()
+            resolve({
+              status: true,
+              message: translate('lib.azure.success'),
+              cosmosData: data
+            })
           } catch (err) {
             reject(err)
           }
