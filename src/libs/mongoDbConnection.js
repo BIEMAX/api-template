ad5de8419a4427e3d4c1de24d7ecfc2959c065f5
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2021-11-16 16:36:13 -0300

    Small change on mongo db connection

--- src/libs/mongoDbConnection.js
+++ src/libs/mongoDbConnection.js
@@ -1,68 +1,35 @@
 'use strict'
 
-const { waterfall, config, translate, moment } = require('./library')
+const { config, translate } = require('./library')
 
 const MongoClient = require('mongodb').MongoClient
+let client = null
 
 /**
  * 
- * @param {String} collection Table/collection name
- * @param {Object} parameters 
- * @param {Date} initialDate 
- * @param {Date} endDate 
  * @returns 
  */
-module.exports = (collection, parameters, initialDate = '', endDate = '') => {
-  return new Promise((resolve, reject) => {
-    waterfall(
-      [
-        (done) => {
-          if (config.database.type.toUpperCase().trim() != 'MONGODB') {
-            done(new Error(translate('lib.conn.mongo')), null)
-          }
-          else done(null)
-        },
-        (done) => {
-          //https://docs.mongodb.com/manual/reference/connection-string
-          //done(null, MongoClient(config.database.connectionString))
-          // },
-          // (conn, done) => {
-          // const test = require('assert');
-          // Connection url
-          // const url = 'mongodb://localhost:27017'
-          // Database Name
-          // const dbName = 'test'
-          // Connect using MongoClient
-          //MongoClient.connect(config.database.connectionString, done)
-          MongoClient.connect(config.database.connectionString, done)
-        },
-        (conn, done) => {
-          //https://gist.github.com/jvadillo/3e856dd1abc4fd57cc76316c7dd0a16f
-          conn
-            .db(config.database.name)
-            .collection(collection)
-            .toArray((result) => {
-              if (conn) conn.close()
-              let payload = {
-                status: true,
-                message: '',
-                data: result
-              }
-              resolve(payload)
-            })
-            .catch((err) => {
-              done(err, conn)
-            })
-          // {
-          //   const db = client.db(dbName);
-          //   client.close();
-          // });
-        }
-      ],
-      (err, conn) => {
-        if (conn) conn.close()
-        reject(err)
-      }
-    )
-  })
+async function connect () {
+  if (config.database.type.toUpperCase().trim() != 'MONGODB') {
+    throw new Error(translate('lib.conn.mongo'))
+  }
+  else {
+    if (!client) client = MongoClient(config.database.connectionString)
+
+    await client.connect(config.database.name)
+    return client.db()
+  }
 }
+
+/**
+ * 
+ * @returns 
+ */
+async function disconnect () {
+  if (!client) return true
+  await client.close()
+  client = null
+  return true
+}
+
+module.exports = { connect, disconnect }
