3bf2539930ce0c183622e95112cbe2f4ca95b327
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2021-11-23 08:23:30 -0300

    Small changes in the module

--- src/libs/groupByObject.js
+++ src/libs/groupByObject.js
@@ -1,18 +1,16 @@
 'use strict'
 
-const { waterfall, translante } = require('./library')
-
-module.exports = () => {
-  return new Promise((resolve, reject) => {
-    waterfall(
-      [
-        (done) => {
-          resolve(true)
-        }
-      ],
-      (err) => {
-        reject(err)
-      }
-    )
+/**
+ * Module that group by an array by a property and return a new array
+ * @param {Array} array Array of objects
+ * @param {String} key Property name to group by
+ * @returns 
+ */
+module.exports = (array, key) => {
+  return array.reduce((acc, obj) => {
+    const property = obj[key]
+    acc[property] = acc[property] || []
+    acc[property].push(obj)
+    return acc
   })
 }
