3fc2ab7b40b7aab085a95357c40b67cd1c074472
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2021-11-29 17:13:35 -0300

    Remove unecessary module and small changes on download files

--- src/libs/downloadFile.js
+++ src/libs/downloadFile.js
@@ -8,17 +8,17 @@ var fs = require('fs')
 /**
  * Download file from URl to destiny
  * @param {String} url File URL 
- * @param {String} destiny Destiny/folder to download the file (e.g.: C:\\temp\\teste.pdf) 
+ * @param {String} folder Destiny/folder to download the file (e.g.: C:\\temp\\teste.pdf) 
  * @returns {Object} Object with information if the download was done/finish
  */
-function downloadFile (url, destiny) {
+function downloadFile (url, folder) {
   return new Promise((resolve, reject) => {
     waterfall(
       [
         (done) => {
           const request = https.get(url, response => {
             if (response.statusCode === 200) {
-              const file = fs.createWriteStream(destiny, { flags: 'wx' })
+              const file = fs.createWriteStream(folder, { flags: 'wx' })
               file.on('finish', () => {
                 resolve({
                   status: true,
@@ -29,14 +29,14 @@ function downloadFile (url, destiny) {
               file.on('error', err => {
                 file.close()
                 if (err.code === 'EEXIST') done(new Error(translate('lib.download.file')))
-                else fs.unlink(destiny, () => done(err.message)) // Delete temp file
+                else fs.unlink(folder, () => done(err.message)) // Delete temp file
               })
               response.pipe(file)
             } else if (response.statusCode === 302 || response.statusCode === 301) {
               //Recursively follow redirects, only a 200 will resolve.
-              downloadFile(response.headers.location, destiny).then((data) => resolve(data))
+              downloadFile(response.headers.location, folder).then((data) => resolve(data))
             } else {
-              fs.unlink(destiny, () => { }) // Delete temp file
+              fs.unlink(folder, () => { }) // Delete temp file
               done(new Error(`Server responded with ${response.statusCode}: ${response.statusMessage}`))
             }
           })
@@ -47,11 +47,11 @@ function downloadFile (url, destiny) {
         }
       ],
       (err) => {
-        newRelic(err, null, null, { url, destino: destiny })
+        newRelic(err, null, null, { url, destino: folder })
         reject(err)
       }
     )
   })
 }
 
-module.exports.downloadFile = downloadFile
+module.exports = { downloadFile }
