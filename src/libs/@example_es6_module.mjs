fee9fd5cc06fef4c70ec6ce56b1adb9cdd587fd1
Dionei Beilke dos Santos <dionei.santos@doctorclin.com.br>
2022-01-12 11:44:12 -0300

    Added sample of es6 nodejs module

---
+++ src/libs/@example_es6_module.mjs
@@ -0,0 +1,12 @@
+export function add () {
+
+}
+
+import myLogger as Logger from 'app.js';
+
+import * as Utils from 'app.js';
+Utils.myLogger();
+
+import Logger2 from 'app.js';
+
+import Logger, { Alligator, myNumbers } from 'app.js';
